bbab645f59f0b0e54f790c7dee2d1a8831c1c962
AyanSwargagit config --global user.name AyanSwarga <you@example.com>
2022-12-08 19:48:03 -0800

    corrected

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,43 +1,24 @@
 #include <stdio.h>
 
-
-
-
-
 /**
-
- * main - Entry point
-
- *
-
- * Return: Always (0) (Success)
-
+ * main - main block
+ * Return: 0
  */
 
-
-/**
- * main - this the starting point of main function
- */
 int main(void)
 
 {
 
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
 
+	printf("Size of an int: %i byte(s)\n", sizeof(int));
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
-
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
 
 	return (0);
-
-
 
 }
