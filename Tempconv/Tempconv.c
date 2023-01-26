a363cb533a5e0386a0050aab751490960ae551b9
AyanSwarga <ayannicholas@gmail.com>
2023-01-26 19:23:19 +0100

    password update

---
+++ Tempconv/Tempconv.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("This is a Temperature Conversion Table from Celsius to Fahrenhit\n");
+
+	float Farh, Celsius
+	int upper, lower, step;
+	lower = 0, upper = 600, step = 20;
+	Celsius = lower
+	for(int lower = 0; lower <= step; lower++)                                                                                          
+	{
+		Fahr = (1.8 * Celsius) + 32;
+		printf("%3f %.2f\n", Fahr, Celsius);
+	}
+	return 0;
+}
