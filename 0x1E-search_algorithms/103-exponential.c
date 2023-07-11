194585099e6aeb5f121dd3cfdfbcd7f1ccd19754
AyanSwarga <ayannicholas@gmail.com>
2023-07-11 20:49:27 +0100

    update

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -43,13 +43,13 @@ int _binary_search(int *array, size_t left, size_t right, int value)
 
 /**
   * exponential_search - Searches for a value in a sorted array
-  *                      of integers using exponential search.
+  * of integers using exponential search.
   * @array: A pointer to the first element of the array to search.
   * @size: The number of elements in the array.
   * @value: The value to search for.
   *
   * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the index where the value is located.
+  * Otherwise, the index where the value is located.
   *
   * Description: Prints a value every time it is compared in the array.
   */
